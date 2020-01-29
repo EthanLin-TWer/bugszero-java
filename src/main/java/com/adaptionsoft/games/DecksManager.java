53368ab9565744b81560ed45e5ecbca5ade06c3d
Linesh <linesh.simpcity@gmail.com>
2020-01-30 05:33:33 +0800

    step-7: move all decks to the manager

--- src/main/java/com/adaptionsoft/games/DecksManager.java
+++ src/main/java/com/adaptionsoft/games/DecksManager.java
@@ -1,4 +1,9 @@
 package com.adaptionsoft.games;
 
 public class DecksManager {
+    final Deck popDeck = new Deck("Pop");
+    final Deck scienceDeck = new Deck("Science");
+    final Deck sportsDeck = new Deck("Sports");
+    final Deck rockDeck = new Deck("Rock");
+
 }
