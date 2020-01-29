fc05c60a8bd8669992b981fb91631160b1df2cff
Linesh <linesh.simpcity@gmail.com>
2020-01-30 05:53:15 +0800

    step-7: replace Deck constructor with Category enum

--- src/main/java/com/adaptionsoft/games/DecksManager.java
+++ src/main/java/com/adaptionsoft/games/DecksManager.java
@@ -1,8 +1,8 @@
 package com.adaptionsoft.games;
 
 public class DecksManager {
-    final Deck popDeck = new Deck("Pop");
-    final Deck scienceDeck = new Deck("Science");
+    final Deck popDeck = new Deck(Category.POP);
+    final Deck scienceDeck = new Deck(Category.SCIENCE);
     final Deck sportsDeck = new Deck("Sports");
     final Deck rockDeck = new Deck("Rock");
 
