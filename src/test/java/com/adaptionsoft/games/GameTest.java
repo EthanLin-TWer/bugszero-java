1f665077be46642be091ce3e5c20987aa7065c5a
Linesh <linesh.simpcity@gmail.com>
2020-01-30 02:30:08 +0800

    step-1: format code

--- src/test/java/com/adaptionsoft/games/GameTest.java
+++ src/test/java/com/adaptionsoft/games/GameTest.java
@@ -9,17 +9,14 @@ import java.util.Random;
 import java.util.stream.IntStream;
 
 public class GameTest {
-
-	@Test
-	public void itsLockedDown() throws Exception {
-
+    @Test
+    public void itsLockedDown() throws Exception {
         Random randomizer = new Random(123455);
         ByteArrayOutputStream resultStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(resultStream));
 
-        IntStream.range(1,15).forEach(i -> GameRunner.playGame(randomizer));
+        IntStream.range(1, 15).forEach(i -> GameRunner.playGame(randomizer));
 
         Approvals.verify(resultStream.toString());
-
-	}
+    }
 }
