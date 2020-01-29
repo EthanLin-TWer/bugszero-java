461a65720935bf7d6e1bcd638cd1349d37bb5b0b
Linesh <linesh.simpcity@gmail.com>
2020-01-30 06:06:31 +0800

    step-7: create categoryDecks hash map

--- src/main/java/com/adaptionsoft/games/DecksManager.java
+++ src/main/java/com/adaptionsoft/games/DecksManager.java
@@ -1,10 +1,17 @@
 package com.adaptionsoft.games;
 
+import java.util.HashMap;
+
 public class DecksManager {
     final Deck popDeck = new Deck(Category.POP);
     final Deck scienceDeck = new Deck(Category.SCIENCE);
     final Deck sportsDeck = new Deck(Category.SPORTS);
     final Deck rockDeck = new Deck(Category.ROCK);
+    final HashMap<Category, Deck> categoryDecks = new HashMap<>();
+
+    public DecksManager() {
+        categoryDecks.put(Category.POP, popDeck);
+    }
 
     public String getNextQuestion(int place) {
         Category[] categories = Category.values();
