1131dbeb86ad8f26c352aa9ed8d44b25535ec9ef
Linesh <linesh.simpcity@gmail.com>
2020-01-30 05:57:10 +0800

    step-7: rename variable back

--- src/main/java/com/adaptionsoft/games/DecksManager.java
+++ src/main/java/com/adaptionsoft/games/DecksManager.java
@@ -7,21 +7,21 @@ public class DecksManager {
     final Deck rockDeck = new Deck(Category.ROCK);
 
     public String getNextQuestion(int place) {
-        Category[] temp_categories = Category.values();
-        String category = temp_categories[place % temp_categories.length].getName();
+        Category[] categories = Category.values();
+        String category = categories[place % categories.length].getName();
 
         System.out.println("The category is " + category);
 
-        if (category.equals(temp_categories[0].getName())) {
+        if (category.equals(categories[0].getName())) {
             return popDeck.getNextQuestion();
         }
-        if (category.equals(temp_categories[1].getName())) {
+        if (category.equals(categories[1].getName())) {
             return scienceDeck.getNextQuestion();
         }
-        if (category.equals(temp_categories[2].getName())) {
+        if (category.equals(categories[2].getName())) {
             return sportsDeck.getNextQuestion();
         }
-        if (category.equals(temp_categories[3].getName())) {
+        if (category.equals(categories[3].getName())) {
             return rockDeck.getNextQuestion();
         }
         throw new Error("");
