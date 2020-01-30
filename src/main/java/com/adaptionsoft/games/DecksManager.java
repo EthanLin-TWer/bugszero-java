be8e401ddacb5f862502684796a571fb8814b7b5
Linesh <linesh.simpcity@gmail.com>
2020-01-30 22:44:29 +0800

    step-7: remove unused code

--- src/main/java/com/adaptionsoft/games/DecksManager.java
+++ src/main/java/com/adaptionsoft/games/DecksManager.java
@@ -1,14 +1,8 @@
 package com.adaptionsoft.games;
 
-import jdk.nashorn.api.tree.ForOfLoopTree;
-
 import java.util.HashMap;
 
 public class DecksManager {
-    final Deck popDeck = new Deck(Category.POP);
-    final Deck scienceDeck = new Deck(Category.SCIENCE);
-    final Deck sportsDeck = new Deck(Category.SPORTS);
-    final Deck rockDeck = new Deck(Category.ROCK);
     final HashMap<Category, Deck> categoryDecks = new HashMap<>();
 
     public DecksManager() {
