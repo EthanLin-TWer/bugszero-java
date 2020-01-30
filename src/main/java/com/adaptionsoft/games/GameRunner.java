74705f243366417cd27f022b3ae912e5a3870030
Linesh <linesh.simpcity@gmail.com>
2020-01-30 23:22:03 +0800

    step-8: extract game.start()

--- src/main/java/com/adaptionsoft/games/GameRunner.java
+++ src/main/java/com/adaptionsoft/games/GameRunner.java
@@ -3,8 +3,6 @@ package com.adaptionsoft.games;
 import java.util.Random;
 
 public class GameRunner {
-	private static boolean notAWinner = true;
-
 	public static void main(String[] args) {
 		Random rand = new Random();
 		playGame(rand);
@@ -17,6 +15,11 @@ public class GameRunner {
 		aGame.add("Pat");
 		aGame.add("Sue");
 
+		start(rand, aGame);
+	}
+
+	private static void start(Random rand, Game aGame) {
+		boolean notAWinner = true;
 		do {
 			aGame.roll(rand.nextInt(5) + 1);
 			final boolean isWrongAnswer = rand.nextInt(9) == 7;
