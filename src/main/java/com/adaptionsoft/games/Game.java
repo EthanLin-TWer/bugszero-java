92ca35128cf509abbf445592cc89e30d89dec954
来一 <tom.lai19871106@gmail.com>
2020-02-02 13:16:08 +0800

    重构：提取 getCurrentPlace 方法

--- src/main/java/com/adaptionsoft/games/Game.java
+++ src/main/java/com/adaptionsoft/games/Game.java
@@ -76,11 +76,11 @@ public class Game {
     }
 
     private void movePlayer(int roll) {
-        places[currentPlayer] = places[currentPlayer] + roll;
-        if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+        places[currentPlayer] = getCurrentPlace() + roll;
+        if (getCurrentPlace() > 11) places[currentPlayer] = getCurrentPlace() - 12;
         System.out.println(getCurrentPlayer()
                 + "'s new location is "
-                + places[currentPlayer]);
+                + getCurrentPlace());
         System.out.println("The category is " + currentCategory());
     }
 
@@ -97,16 +97,20 @@ public class Game {
 
 
     private String currentCategory() {
-        if (places[currentPlayer] == 0) return "Pop";
-        if (places[currentPlayer] == 4) return "Pop";
-        if (places[currentPlayer] == 8) return "Pop";
-        if (places[currentPlayer] == 1) return "Science";
-        if (places[currentPlayer] == 5) return "Science";
-        if (places[currentPlayer] == 9) return "Science";
-        if (places[currentPlayer] == 2) return "Sports";
-        if (places[currentPlayer] == 6) return "Sports";
-        if (places[currentPlayer] == 10) return "Sports";
+        if (getCurrentPlace() == 0) return "Pop";
+        if (getCurrentPlace() == 4) return "Pop";
+        if (getCurrentPlace() == 8) return "Pop";
+        if (getCurrentPlace() == 1) return "Science";
+        if (getCurrentPlace() == 5) return "Science";
+        if (getCurrentPlace() == 9) return "Science";
+        if (getCurrentPlace() == 2) return "Sports";
+        if (getCurrentPlace() == 6) return "Sports";
+        if (getCurrentPlace() == 10) return "Sports";
         return "Rock";
+    }
+
+    private int getCurrentPlace() {
+        return places[currentPlayer];
     }
 
     public boolean wasCorrectlyAnswered() {
