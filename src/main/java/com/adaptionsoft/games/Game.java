eb53f63c5bf7223c5c6c07ba797bd5366c052f6d
来一 <tom.lai19871106@gmail.com>
2020-02-02 12:47:25 +0800

    重构：修改拼写错误 corrent -> correct

--- src/main/java/com/adaptionsoft/games/Game.java
+++ src/main/java/com/adaptionsoft/games/Game.java
@@ -125,7 +125,7 @@ public class Game {
 
 		} else {
 
-			System.out.println("Answer was corrent!!!!");
+			System.out.println("Answer was correct!!!!");
 			goldCoins[currentPlayer]++;
 			System.out.println(players.get(currentPlayer)
 					+ " now has "
