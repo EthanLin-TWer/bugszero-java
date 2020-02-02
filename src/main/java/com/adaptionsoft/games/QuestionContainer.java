924b0a8138523f2ef59c3b79d9d5c3fce228710e
来一 <tom.lai19871106@gmail.com>
2020-02-02 17:18:06 +0800

    重构：在 QuestionContainer 类中创建构造器

--- src/main/java/com/adaptionsoft/games/QuestionContainer.java
+++ src/main/java/com/adaptionsoft/games/QuestionContainer.java
@@ -12,6 +12,17 @@ import java.util.LinkedList;
 public class QuestionContainer {
     private HashMap<Category, LinkedList<String>> questionMap = new HashMap<>();
 
+    public QuestionContainer() {
+        for (Category category : Category.values()) {
+            LinkedList<String> list = new LinkedList<>();
+            for (int i = 0; i < 50; i++) {
+                String question = category.getValue() + " Question " + i;
+                list.addLast(question);
+            }
+            questionMap.put(category, list);
+        }
+    }
+
     public String getNextQuestion(Category category) {
         return questionMap.get(category).removeFirst();
     }
