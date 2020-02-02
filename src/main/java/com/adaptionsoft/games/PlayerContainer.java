912615a083d7ac5999769edd9aad600d4a83706f
来一 <tom.lai19871106@gmail.com>
2020-02-02 16:54:20 +0800

    重构：在 PlayerContainer 类中创建 nextPlayer 方法

--- src/main/java/com/adaptionsoft/games/PlayerContainer.java
+++ src/main/java/com/adaptionsoft/games/PlayerContainer.java
@@ -10,9 +10,15 @@ import java.util.ArrayList;
  **/
 public class PlayerContainer {
     private ArrayList<Player> players = new ArrayList<>();
+    private int currentPlayer = 0;
 
     public void addPlayer(String playerName) {
         players.add(new Player(playerName));
         System.out.println("They are player number " + players.size());
+    }
+
+    private void nextPlayer() {
+        currentPlayer++;
+        if (currentPlayer == players.size()) currentPlayer = 0;
     }
 }
