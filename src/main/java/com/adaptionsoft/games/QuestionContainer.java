3b260cbc7312420efbe127fe6eba4f474e764d44
来一 <tom.lai19871106@gmail.com>
2020-02-02 17:14:28 +0800

    重构：创建 QuestionContainer 类，封装question HashMap

---
+++ src/main/java/com/adaptionsoft/games/QuestionContainer.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lai.yi
+ * Date: 2020/2/2
+ * Description:
+ **/
+public class QuestionContainer {
+    private HashMap<Category, LinkedList<String>> questionMap = new HashMap<>();
+}
